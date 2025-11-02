5550ace193b9fc08f1f0cfc18e39ed0b368161f5
RayL-L <ray.lukerui@gmail.com>
2025-11-01 22:33:17 -0400

    Refactor StudyTimer class and update timer logic

--- timer.py
+++ timer.py
@@ -1,59 +1,63 @@
 import time, json, datetime
 
-TODAY = datetime.date.today()
-FILE = "timer_data.json"
-DETAIL = "detail_data.json"
-data_timer = json.load(open(FILE))
-data_detail = json.load(open(DETAIL))
+class StudyTimer:
+    def __init__(self, file_path: str = "timer_data.json", detail_path: str = "detail_data.json") -> None:
+        self.FILE = file_path
+        self.DETAIL = detail_path
 
-ifEnd = False
-ifPause = False
 
-focusTime = []
 
-input("press return to start: ")
-start = time.time()
+    def run(self) -> None:
+        TODAY = datetime.date.today()
+        FILE = self.FILE
+        DETAIL = self.DETAIL
 
-while ifEnd == False:
-    a = input("pause to pause, resume to resume: ")
-    if a == "pause":
-        end = time.time()
-        duration = round((end - start) , 1)
-        focusTime.append(duration)
-        ifPause = True
-    elif a == "resume" and ifPause == True:
+        data_timer = json.load(open(FILE))
+        data_detail = json.load(open(DETAIL))
+
+        ifEnd = False
+        ifPause = False
+
+        focusTime = []
+
+        input("press return to start: ")
         start = time.time()
-        ifPause = False 
-    elif a == "":
-        if ifPause:
-            ifEnd = True
-            break
-        else:
-            end = time.time()
-            duration = round((end - start) , 1)
-            focusTime.append(duration)
-            ifEnd = True
-            break
 
-# print(f"{duration:.1f} seconds.")
+        while ifEnd == False:
+            a = input("pause to pause, resume to resume: ")
+            if a == "pause":
+                end = time.time()
+                duration = round((end - start), 1)
+                focusTime.append(duration)
+                ifPause = True
+            elif a == "resume" and ifPause == True:
+                start = time.time()
+                ifPause = False
+            elif a == "":
+                if ifPause:
+                    ifEnd = True
+                    break
+                else:
+                    end = time.time()
+                    duration = round((end - start), 1)
+                    focusTime.append(duration)
+                    ifEnd = True
+                    break
 
-data_timer.append(
-    {"date": str(TODAY),
-    "duration_sec": round(sum(focusTime), 1)}
-    )
+        # print(f"{duration:.1f} seconds.")
 
-data_detail.append(
-    {"date": str(TODAY),
-    "duration_details": focusTime}
-    )
+        data_timer.append(
+            {"date": str(TODAY),
+             "duration_sec": round(sum(focusTime), 1)}
+        )
 
-with open(FILE, "w") as f:
-    json.dump(data_timer, f)
+        data_detail.append(
+            {"date": str(TODAY),
+             "duration_details": focusTime}
+        )
 
-with open(DETAIL, "w") as f:
-    json.dump(data_detail, f)
+        with open(FILE, "w") as f:
+            json.dump(data_timer, f)
 
-
-
-
-
+        with open(DETAIL, "w") as f:
+            json.dump(data_detail, f)
